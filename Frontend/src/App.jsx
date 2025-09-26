2a0a0a7b0937cea8bd61fb2b3a45f4acce84c828
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-09-26 12:23:41 +0530

    CORS setup and return response

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,22 @@
+import axios from "axios"
 import prism from "prismjs"
 import "prismjs/themes/prism-tomorrow.css"
-import { useEffect } from "react"
+import Editor from "react-simple-code-editor";
+import { useEffect, useState } from "react"
 
 const App = () => {
+
+  const [code, setcode] = useState(`Enter your code here...`)
+  const [review, setreview] = useState(``)
 
   useEffect(() => {
     prism.highlightAll()
   },)
+
+  async function reviewcode(){
+    const request = await axios.post("http://localhost:3000/ai/get-review", { code })
+    setreview(request.data)
+  }
   
   return (
     <>
@@ -17,17 +27,21 @@ const App = () => {
         <div className="conatiner p-5 h-[95%] w-full flex gap-8">
           <div className="left h-full w-[50%] relative">
             <div className="code h-full w-full">
-              <pre className="rounded-2xl h-full w-full">
-                <code className="language-javascript">{`function sum(){
-                return 1+1
-                }`}</code>
-              </pre>
+              <Editor
+                value={code}
+                onValueChange={(code) => setcode(code)}
+                highlight={(code) => prism.highlight(code, prism.languages.javascript, 'javascript')}
+                padding={10}
+                className="bg-zinc-800 text-white rounded-2xl font-mono h-full w-full outline-none"
+              />
             </div>
-            <button className="absolute select-none bg-[#f2f0ef94] px-5 py-2 rounded-lg bottom-8 right-8">
+            <button onClick={reviewcode} className="absolute select-none bg-[#f2f0ef94] px-5 py-2 rounded-lg bottom-8 right-8">
               Review
             </button>
           </div>
-          <div className="right h-full w-[50%] bg-zinc-800 rounded-2xl m-[0.5em] pt-[1em] pb-[1em]"></div>
+          <div className="right h-full w-[50%] bg-zinc-800 rounded-2xl text-white">
+            {review}
+          </div>
         </div>
       </main>
     </>
